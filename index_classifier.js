9e6fd46b7dabd6b90b15df6e197d241283155f6f
jyotisman-ds <jsahoo@iastate.edu>
2021-01-12 20:05:18 -0600

    javascript to implement a browser tfjs model

---
+++ index_classifier.js
@@ -0,0 +1,50 @@
+let model_beans;
+const img = document.getElementById('img');
+const outp = document.getElementById('output');
+const tensorFeature = tf.browser.fromPixels(img).resizeBilinear([150,150]).expandDims();
+let isPredicting = false;
+    
+async function loadBeans() {
+    const MODEL_URL = 'http://127.0.0.1:8887/model.json';
+    const model_beans = await tf.loadLayersModel(MODEL_URL);
+    return tf.model({inputs: model_beans.inputs, outputs: model_beans.output});
+}
+    
+async function predict(){
+
+    if (isPredicting) {
+            const predictedClass = tf.tidy(() => {
+            const predictions = model_beans.predict(tensorFeature);
+            return predictions.as1D().argMax();
+            });
+            const classId = (await predictedClass.data())[0];
+        
+            switch(classId){
+		    case 0:
+			 predictionText = "Angular leaf spot(0)";
+            break;
+		    case 1:
+			 predictionText = "Bean rust(1)";
+			break;
+		    case 2:
+			 predictionText = "Healthy(2)";
+			break;
+            }
+            document.getElementById("prediction").innerText = predictionText;
+    
+            predictedClass.dispose();
+    }
+}
+    
+function startPredicting(){
+	isPredicting = true;
+	predict();
+}
+
+
+async function init(){
+	   model_beans = await loadBeans();
+       console.log(model_beans.summary());
+}
+
+init();
